370866134b3a7b46249b4e5b9c6ce4171ccf7344
wsiel <jofielsilva2001@gmail.com>
2023-09-01 17:34:41 +0200

    Third Project completed

--- projects/03-Mouse_Follower/src/App.jsx
+++ projects/03-Mouse_Follower/src/App.jsx
@@ -1,5 +1,53 @@
+import { useEffect, useState } from 'react'
+
+const FollowMouse = () => {
+	const [enabled, setEnabled] = useState(false)
+	const [position, setPosition] = useState({ x: 0, y: 0 })
+
+	useEffect(() => {
+		const handleMove = (event) => {
+			const { clientX, clientY } = event
+			setPosition({ x: clientX, y: clientY })
+		}
+		if (enabled) {
+			window.addEventListener('pointermove', handleMove)
+		}
+
+		return () => {
+			console.log('cleanup')
+			window.removeEventListener('pointermove', handleMove)
+		}
+	}, [enabled])
+
+	return (
+		<>
+			<div
+				style={{
+					position: 'absolute',
+					backgroundColor: '#09f',
+					borderRadius: '50%',
+					opacity: 0.8,
+					pointerEvents: 'none',
+					left: -20,
+					top: -20,
+					width: 40,
+					height: 40,
+					transform: `translate(${position.x}px, ${position.y}px)`,
+				}}
+			/>
+			<button onClick={() => setEnabled(!enabled)}>
+				{enabled ? 'On' : 'Off'}
+			</button>
+		</>
+	)
+}
+
 function App() {
-	return <h3>Project 3</h3>
+	return (
+		<main>
+			<FollowMouse />
+		</main>
+	)
 }
 
 export default App
