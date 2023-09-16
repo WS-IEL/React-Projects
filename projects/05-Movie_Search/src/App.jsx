8b2d62101b414b644abd15d436b684da223a3c05
wsiel <jofielsilva2001@gmail.com>
2023-09-16 04:10:04 +0200

    minor changes to appjsx

--- projects/05-Movie_Search/src/App.jsx
+++ projects/05-Movie_Search/src/App.jsx
@@ -3,20 +3,27 @@ import { useMovies } from './hooks/useMovies.js'
 import { Movies } from './components/Movies.jsx'
 
 function App() {
-	const { movies: mappedMovies } = useMovies()
+	const { movies } = useMovies()
+
+	const handleSubmit = (event) => {
+		event.preventDefault()
+		const fields = new window.FormData(event.target)
+		const query = fields.get('query')
+		console.log(query)
+	}
 
 	return (
 		<div className='page'>
 			<header>
 				<h1>Movie Search</h1>
-				<form className='form'>
-					<input placeholder='Avengers, Avatar, Jumanji...' />
+				<form className='form' onSubmit={handleSubmit}>
+					<input name='query' placeholder='Avengers, Avatar, Jumanji...' />
 					<button type='submit'>Search</button>
 				</form>
 			</header>
 
 			<main>
-				<Movies movies={mappedMovies} />
+				<Movies movies={movies} />
 			</main>
 		</div>
 	)
